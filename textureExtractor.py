0f0fe01a07ca343a4f60bd5fe77d8d09a11a423a
linuxclimber <7259447+linuxclimber@users.noreply.github.com>
2021-05-07 12:49:16 -0400

    Intial Commit

---
+++ textureExtractor.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+
+import os.path
+from os import path
+
+from PIL import Image
+import numpy as np
+
+
+def main():
+    if not path.exists("TEXDIC.htd"):
+        print("TEXDIC.htd not found!  Please copy a TEXDIC file into this folder to continue")
+        exit()
+    textureName = input("Enter the texture name you want to extract: ")
+    extractTexture(textureName)
+    
+def extractTexture(textureName):
+    print("Attempting to extract " + textureName + " from TEXDIC.htd")
+    f = open("TEXDIC.htd", "rb")
+    matchedBytes = 0
+    
+    while matchedBytes < (len(textureName)):
+        currByte = f.read(1)
+        if not currByte:
+            break #eof
+
+        if currByte == textureName[matchedBytes].encode('utf-8'):
+            matchedBytes += 1
+        else:
+            matchedBytes = 0
+
+    if matchedBytes >= (len(textureName)) and f.read(1).hex() == "00":
+        print("Successfully found texture '" + textureName + "' in file! Extracting...")
+        #Skip over the unknown metadata stuff
+        prevBytes = []
+        consecutiveNulls = 0
+        while consecutiveNulls < 4:
+            currByte = f.read(1)
+
+            if not currByte:
+                break #eof
+            
+            if currByte.hex() == "00":
+                consecutiveNulls += 1
+            else:
+                consecutiveNulls = 0
+            prevBytes.append(currByte)
+            
+        #Read the height/width
+        width = int.from_bytes(b''.join(prevBytes[-16:-12]), byteorder='big')
+        height = int.from_bytes(b''.join(prevBytes[-12:-8]), byteorder='big')
+        print("Texture Size: " + str(width) + "x" + str(height))
+
+        if width <=0 or height <= 0:
+            print("Invalid size.  Skipping")
+            return
+        
+        if width > 1920 or height > 1080:
+            print("Texture size too large. Skipping")
+            return
+
+        if b''.join(prevBytes[-8:-4]).hex() != "0000000e":
+            print("Not a CMPR image- may not extract properly")
+            return
+        
+        imageBytes = (width*height)//2
+        imageData = f.read(imageBytes)
+        f.read(32)
+        createPNG(width, height, imageData, textureName)
+    else:
+        print("Failed to find texture '" + textureName + "' in file!")
+            
+def createPNG(width, height, imageData, fileName):
+    global subBlockNum
+    global blockNum
+    subBlockNum = 0
+    blockNum = 0
+
+    full = [0]*3
+    full = [full]*width
+    full = [full]*height
+    full = np.array(full, dtype=np.uint8)
+    
+    for x in range(height//8):
+        for y in range(width//8):
+            imageData, block = generateNextBlock(imageData)
+            for blockx in range(8):
+                for blocky in range(8):
+                    full[(x*8) + blockx, (y*8) + blocky, 0] = block[blockx,blocky,0]
+                    full[(x*8) + blockx, (y*8) + blocky, 1] = block[blockx,blocky,1]
+                    full[(x*8) + blockx, (y*8) + blocky, 2] = block[blockx,blocky,2]
+
+    new_image = Image.fromarray(full, "RGB")
+    new_image.save("textures/" + fileName + ".png")
+
+def generateNextBlock(imageData):
+    global blockNum
+    blockNum += 1
+    block=[0]*3
+    block=[block]*8
+    block=[block]*8
+    block = np.array(block, dtype=np.uint8)
+    for x in range(2):
+        for y in range(2):
+            imageData, subBlock = generateSubBlock(imageData)
+            for subx in range(4):
+                for suby in range(4):
+                    block[(x*4) + subx, (y*4) + suby, 0] = subBlock[subx,suby,0]
+                    block[(x*4) + subx, (y*4) + suby, 1] = subBlock[subx,suby,1]
+                    block[(x*4) + subx, (y*4) + suby, 2] = subBlock[subx,suby,2]
+    return imageData, block
+
+def generateSubBlock(imageData):
+    global subBlockNum
+    subBlockNum += 1
+    sub=[0]*3
+    sub=[sub]*4
+    sub=[sub]*4
+    sub = np.array(sub, dtype=np.uint8)
+
+    color0 = convertRGB(int.from_bytes(imageData[0:2], byteorder="big"))
+    color1 = convertRGB(int.from_bytes(imageData[2:4], byteorder="big"))
+    color2, color3 = getOtherColors(color0, color1)
+    colors = [color0, color1, color2, color3]
+
+    for y in range(4):
+        rowByte = imageData[4+y]
+        for x in range(4):
+            RGB565 = colors[(rowByte >> ((3-x)*2) & 0b11)]
+            sub[y,x,0] = RGB565[0]
+            sub[y,x,1] = RGB565[1]
+            sub[y,x,2] = RGB565[2]
+    return imageData[8:], sub
+
+
+def convertRGB(RGB565):
+    R = (RGB565 & 0b1111100000000000) >> 8
+    G = (RGB565 & 0b0000011111100000) >> 3
+    B = (RGB565 & 0b0000000000011111) << 3
+    return [R, G, B]
+
+def getOtherColors(c0, c1):
+    c2 = [0,0,0]
+    c3 = [0,0,0]
+    if c0 > c1:
+        for i in range(3):
+            interval = abs(c0[i]-c1[i])/3
+            c2[i] = round(min(c0[i], c1[i]) + interval)
+            c3[i] = round(max(c0[i], c1[i]) - interval)
+    else:
+        for i in range(3):
+            c2[i] = (c0[i] + c1[i])//2
+
+    return c2, c3
+
+if __name__ == "__main__":
+    main()
---
+++ textureFinder.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+import os.path
+from curses.ascii import isalpha, isdigit
+from os import path, mkdir, rename
+import textureExtractor as texExtract
+
+def main():
+    if not path.exists("TEXDIC.htd"):
+        print("TEXDIC.htd not found!  Please copy a TEXDIC file into this folder to continue")
+        exit()
+    textures = findCMPRTextures()
+    print(textures)
+    for tName in (textures):
+        texExtract.extractTexture(tName)
+    newFolderName = input("Enter desired texture folder name: ")
+    rename("textures", newFolderName)
+    mkdir("textures")
+
+def findCMPRTextures():
+    f = open("TEXDIC.htd", "rb")
+    textures = []
+    prevBytes = []
+    while True:
+        currByte = f.read(1)
+        if not currByte:
+            break #eof
+
+        prevBytes.append(currByte)
+        if b''.join(prevBytes[-8:]).hex() == "0000000e00000000":
+            index = -17
+            #print("CMPR Texture found.  Getting filename now")
+            while len(prevBytes)+index > 1 and index > -70:
+                #print(prevBytes[index].hex)
+                if prevBytes[index].hex() == "00":
+                    #print("Checking if filename")
+                    isFilename, startIndex = fnCheck(prevBytes, index)
+                    if isFilename:
+                        #print(b''.join(prevBytes[startIndex:index]))
+                        textures.append(b''.join(prevBytes[startIndex:index]).decode("utf-8"))
+                        prevBytes = []
+                        break
+                index -= 1
+        
+    return textures
+
+def fnCheck(prevBytes, index):
+    #print("Checking filename")
+    i = index - 1
+    matchedBytes = 0
+    while len(prevBytes)+i > 1:
+        currByte = prevBytes[i]
+        #print("currByte: " + str(currByte))
+        if isalpha(int.from_bytes(currByte, "big")) or isdigit(int.from_bytes(currByte, "big")) or currByte.hex() == "5f":
+            matchedBytes += 1
+            #print(b''.join(prevBytes[i:index]))
+        else:
+            if matchedBytes >= 6:
+                return True, i+1
+            return False, i
+        i -= 1
+    return False, i
+
+if __name__ == "__main__":
+    main()
