d60bd96e09a5671f9756626ed15499149bbae077
LinuxClimber <7259447+linuxclimber@users.noreply.github.com>
2021-05-08 15:53:24 -0400

    Removed extraneous debug print statements

--- textureFinder.py
+++ textureFinder.py
@@ -29,14 +29,10 @@ def findCMPRTextures():
         hexStr = b''.join(prevBytes[-8:]).hex()
         if hexStr == "0000000e00000000" or hexStr == "0000000600000000":
             index = -17
-            #print("CMPR Texture found.  Getting filename now")
             while len(prevBytes)+index > 1 and index > -100:
-                #print(prevBytes[index].hex)
                 if prevBytes[index].hex() == "00":
-                    #print("Checking if filename")
                     isFilename, startIndex = fnCheck(prevBytes, index)
                     if isFilename:
-                        #print(b''.join(prevBytes[startIndex:index]))
                         textures.append(b''.join(prevBytes[startIndex:index]).decode("utf-8"))
                         prevBytes = []
                         break
@@ -45,15 +41,12 @@ def findCMPRTextures():
     return textures
 
 def fnCheck(prevBytes, index):
-    #print("Checking filename")
     i = index - 1
     matchedBytes = 0
     while len(prevBytes)+i > 1:
         currByte = prevBytes[i]
-        #print("currByte: " + str(currByte))
         if islower(int.from_bytes(currByte, "big")) or isdigit(int.from_bytes(currByte, "big")) or currByte.hex() == "5f":
             matchedBytes += 1
-            #print(b''.join(prevBytes[i:index]))
         else:
             if matchedBytes >= 2:
                 return True, i+1
